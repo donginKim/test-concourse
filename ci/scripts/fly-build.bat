b05026a134ab38a56dc478937ad9540408ca95dc
ubuntu <steve1145@naver.com>
2019-07-12 06:33:11 +0900

    test

---
+++ ci/scripts/fly-build.bat
@@ -0,0 +1,12 @@
+set PATH=C:\Go\bin;%PATH%
+
+set GOPATH=%CD%\concourse
+set PATH=%CD%\concourse\bin;%PATH%
+
+set /p FinalVersion=<final-version\version
+
+go build -ldflags "-X github.com/concourse/fly/version.Version=%FinalVersion%" -o fly_windows_amd64.exe github.com/concourse/fly
+
+powershell.exe Copy-Item fly_windows_amd64.exe windows-binary/
+
+certUtil -hashfile .\windows-binary\fly_windows_amd64.exe SHA1 > .\windows-binary\fly_windows_amd64.exe.sha1
